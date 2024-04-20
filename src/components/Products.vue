0740256e4bac45471223978ea676186962f60f23
ganesh bhatt <@ganesh.bhatta>
2024-04-20 19:16:49 +0545

    UI: some design of card, button, reset added...

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -6,62 +6,94 @@ const props = defineProps({
   data: {
     type: Array,
     required: true
-  }});
+  }
+});
 
-  const selectedProduct = ref(null);
-  const isOpen = ref(false);
+const selectedProduct = ref(null);
+const isOpen = ref(false);
 console.log(isOpen, open)
-  const openModal = (product) => {
-    selectedProduct.value = product;
-    isOpen.value = true;
-  };
+const openModal = (product) => {
+  selectedProduct.value = product;
+  isOpen.value = true;
+};
 
-  const closeModal = () => {
-    isOpen.value = false;
-  };
+const closeModal = () => {
+  isOpen.value = false;
+};
 
 </script>
 
 <template>
- <div class='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-2'>
-  <div v-for='product in props.data' :key='product?.id' @click='openModal(product)'>
-    <div class='img flex justify-center items-center'>
-      <img :src="product?.image" alt="pro-image" />
-    </div>
-    <div class='flex flex-col'>
-      RS. {{ product?.price }}
-      <div class='flex justify-between'>
-        <div class='flex'>
-          <span>Rating:</span> <h6>{{ product?.rating?.rate }}</h6>
-        </div>
-        <div class='flex'>
-          <span>Remaining:</span> <p>{{ product?.rating?.count }}</p>
+  <div class='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+    <div v-for='product in props.data' :key='product?.id'
+      class='bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <div class='img flex justify-center items-center'>
+        <img :src="product?.image" alt="pro-image" class='rounded-t-lg' />
+      </div>
+      <div class='flex flex-col mt-2 text-center'>
+        <h5>
+          RS. {{ product?.price }}
+        </h5>
+        <div class='flex justify-between px-3'>
+          <div class='flex items-center'>
+            <span>Rating: </span>
+            <h6 class='text-gray-500'>{{ product?.rating?.rate }}</h6>
+          </div>
+          <div class='flex items-center'>
+            <span>Remaining: </span>
+            <p class='text-gray-500'>{{ product?.rating?.count }}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div>
-      {{ product?.title }}
-    </div>
-    <div>
-      {{ product?.description.slice(0, 20) }}
+      <div class='px-3 text-black-800'>
+        <h6>
+          {{ product?.title.slice(0, 20) }}...
+        </h6>
+      </div>
+      <div class='px-3'>
+        <p>
+          {{ product?.description.slice(0, 50) }}...
+        </p>
+      </div>
+      <div class="flex justify-center items-center">
+        <button @click="openModal(product)" class="px-4 py-2 text-black hover:bg-lime-600 hover:text-white border rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 hover:border-lime-900">
+    View
+</button>
+
+      </div>
+
     </div>
   </div>
- </div>
 
- <ViewModal :isOpen="isOpen" :product="selectedProduct" :closeModal="closeModal" />
+  <ViewModal :isOpen="isOpen" :product="selectedProduct" :closeModal="closeModal" />
 </template>
 
 
 <style>
-.container{
+.container {
   height: 60vh;
 }
-.img{
+
+.img {
   height: 30vh;
   width: 240px;
 }
-.img img{
+
+.img img {
   height: 100%;
   width: 100%;
 }
-</style>
+
+p {
+  font-size: 16px;
+}
+
+h5 {
+  font-size: 24px;
+  font-weight: 700;
+}
+
+h6 {
+  font-size: 19px;
+  font-weight: 500;
+}</style>
--- src/pages/productList.vue
+++ src/pages/productList.vue
@@ -119,13 +119,16 @@ function selectSuggestion(suggestion) {
             </select>
 
             <select v-model="price"
-                class='mb-2 px-4 py-2 rounded border border-gray-500 focus:outline-none focus:border-blue-500'>
-                <option disabled value="">Price</option>
+                class='w-55 mb-2 px-4 py-2 rounded border border-gray-500 focus:outline-none focus:border-blue-500'>
+                <option disabled value="">Select Price</option>
                 <option value="ascending">High</option>
                 <option value="descending">Low</option>
             </select>
             <button @click="resetFilters"
-                class='mb-2 px-4 py-2 rounded border border-gray-500 focus:outline-none hover:bg-blue-500 hover:text-white'>Reset</button>
+        class="mb-2 px-4 py-2 rounded border border-red-500 text-red-400 focus:outline-none hover:bg-red-500 hover:text-white transition-colors duration-300">
+    Reset
+</button>
+
         </div>
         <div v-if="loading">Loading...</div>
         <div v-if="error">Error: {{ error }}</div>
